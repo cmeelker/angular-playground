7d73e2fb4a1f3d51520f9cc146e582f90781091f
ChristaMMeelker <christa@infi.nl>
2024-06-27 14:09:33 +0200

    Add icons and styling

--- src/app/components/catalog/card/card.component.ts
+++ src/app/components/catalog/card/card.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CatalogItem } from '../../../domain/CatalogItem';
 import { CurrencyPipe } from '@angular/common';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'bakery-card',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, FontAwesomeModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
 export class CardComponent {
   @Input() item!: CatalogItem;
+  faCirclePlus = faCirclePlus;
 }
--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'bakery-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  faCartShopping = faCartShopping;
 }
