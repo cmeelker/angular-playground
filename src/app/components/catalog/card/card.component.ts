7ceeed5f0df4536a5743bcc838abee487027b28e
ChristaMMeelker <christa@infi.nl>
2024-06-27 13:46:53 +0200

    Style cards

--- src/app/components/catalog/card/card.component.ts
+++ src/app/components/catalog/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CatalogItem } from '../../../domain/CatalogItem';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'bakery-card',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
