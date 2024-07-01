e91aa775fec80c3e0d499fc68aee99f92df05ee6
ChristaMMeelker <christa@infi.nl>
2024-07-01 15:36:15 +0200

    Move app component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 
 @Component({
   selector: 'bakery-root',
